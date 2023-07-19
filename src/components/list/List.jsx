203623a78c072af5511aaca10ac60093c785da53
Rohit <prjagtap90@gmail.com>
2023-07-19 20:31:20 +0530

    Addeed Slide Effect

---
+++ src/components/list/List.jsx
@@ -0,0 +1,55 @@
+import "./list.scss";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import Listitem from "../listitem/Listitem";
+import { useRef, useState } from "react";
+
+const List = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef();
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosNewOutlinedIcon
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+          <Listitem />
+        </div>
+        <ArrowForwardIosOutlinedIcon
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default List;
