4f251a56eb12feb8ab8172b44a56fb7ead477270
Rohit <prjagtap90@gmail.com>
2023-07-19 22:13:57 +0530

    added list items 126

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -16,7 +16,7 @@ const List = () => {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
-    if (direction === "right" && slideNumber < 5) {
+    if (direction === "right" && slideNumber < 2) {
       setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
@@ -31,17 +31,16 @@ const List = () => {
           style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
-          <Listitem />
+          <Listitem index={0} />
+          <Listitem index={1} />
+          <Listitem index={2} />
+          <Listitem index={3} />
+          <Listitem index={4} />
+          <Listitem index={5} />
+          <Listitem index={6} />
+          <Listitem index={7} />
+          <Listitem index={8} />
+          <Listitem index={9} />
         </div>
         <ArrowForwardIosOutlinedIcon
           className="sliderArrow right"
